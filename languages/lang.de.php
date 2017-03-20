28edb1d5015e8587cfaabcfa869d297825e3589b
dannyk660 <contact@dakboard.com>
2017-03-20 16:03:35 -0400

    Initial commit of other language files.

---
+++ languages/lang.de.php
@@ -0,0 +1,60 @@
+<?php
+/* 
+------------------
+Language: German
+------------------
+*/
+
+$lang = array();
+
+// Days of the week
+$lang['monday'] = 'Monday';
+$lang['monday_abbr'] = 'Mon';
+$lang['tuesday'] = 'Tuesday';
+$lang['tuesday_abbr'] = 'Tue';
+$lang['wednesday'] = 'Wednesday';
+$lang['wednesday_abbr'] = 'Wed';
+$lang['thursday'] = 'Thursday';
+$lang['thursday_abbr'] = 'Thu';
+$lang['friday'] = 'Friday';
+$lang['friday_abbr'] = 'Fri';
+$lang['saturday'] = 'Saturday';
+$lang['saturday_abbr'] = 'Sat';
+$lang['sunday'] = 'Sunday';
+$lang['sunday_abbr'] = 'Sun';
+
+// Months of the year
+$lang['january'] = 'January';
+$lang['january_abbr'] = '';
+$lang['february'] = 'February';
+$lang['february_abbr'] = 'Feb';
+$lang['march'] = 'March';
+$lang['march_abbr'] = 'Mar';
+$lang['april'] = 'April';
+$lang['april_abbr'] = 'Apr';
+$lang['may'] = 'May';
+$lang['may_abbr'] = 'May';
+$lang['june'] = 'June';
+$lang['june_abbr'] = 'Jun';
+$lang['july'] = 'July';
+$lang['july_abbr'] = 'Jul';
+$lang['august'] = 'August';
+$lang['august_abbr'] = 'Aug';
+$lang['september'] = 'September';
+$lang['september_abbr'] = 'Sep';
+$lang['october'] = 'October';
+$lang['october_abbr'] = 'Oct';
+$lang['november'] = 'November';
+$lang['november_abbr'] = 'Nov';
+$lang['december'] = 'December';
+$lang['december_abbr'] = 'Dec';
+
+// Other terms used
+$lang['today'] = 'Today';
+$lang['tomorrow'] = 'Tomorrow';
+$lang['all_day'] = 'All day';
+$lang['calendar'] = 'Calendar';
+$lang['weather'] = 'Weather';
+$lang['todo'] = 'To-do';
+$lang['loading'] = 'Loading';
+?>
