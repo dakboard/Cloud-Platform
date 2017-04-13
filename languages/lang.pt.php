1caf3f7ff539da3ceb4f2fc36d34b7d24a375d6c
Celso Martins <eu@celsomartins.com>
2017-04-12 22:28:00 -0300

    Portuguese Language
    New Language Portuguese (I'm from Brazil, so it's pt-br, but there are
    no changes comparing to pt from Portugal. In this case, the words are
    the same).

---
+++ languages/lang.pt.php
@@ -0,0 +1,61 @@
+<?php
+/* 
+--------------------
+Language: Portuguese
+--------------------
+*/
+
+$lang = array();
+
+// Days of the week
+$lang['monday'] = 'Segunda-feira';
+$lang['monday_abbr'] = 'Seg';
+$lang['tuesday'] = 'Terça-feira';
+$lang['tuesday_abbr'] = 'Ter';
+$lang['wednesday'] = 'Quarta-feira';
+$lang['wednesday_abbr'] = 'Qua';
+$lang['thursday'] = 'Quinta-feira';
+$lang['thursday_abbr'] = 'Qui';
+$lang['friday'] = 'Sexta-feira';
+$lang['friday_abbr'] = 'Sex';
+$lang['saturday'] = 'Sábado';
+$lang['saturday_abbr'] = 'Sab';
+$lang['sunday'] = 'Domingo';
+$lang['sunday_abbr'] = 'Dom';
+
+// Months of the year
+$lang['january'] = 'Janeiro';
+$lang['january_abbr'] = 'Jan';
+$lang['february'] = 'Fevereiro';
+$lang['february_abbr'] = 'Fev';
+$lang['march'] = 'Março';
+$lang['march_abbr'] = 'Mar';
+$lang['april'] = 'Abril';
+$lang['april_abbr'] = 'Abr';
+$lang['may'] = 'Maio';
+$lang['may_abbr'] = 'Mai';
+$lang['june'] = 'Junho';
+$lang['june_abbr'] = 'Jun';
+$lang['july'] = 'Julho';
+$lang['july_abbr'] = 'Jul';
+$lang['august'] = 'Agosto';
+$lang['august_abbr'] = 'Ago';
+$lang['september'] = 'Setembro';
+$lang['september_abbr'] = 'Set';
+$lang['october'] = 'Outubro';
+$lang['october_abbr'] = 'Out';
+$lang['november'] = 'Novembro';
+$lang['november_abbr'] = 'Nov';
+$lang['december'] = 'Dezembro';
+$lang['december_abbr'] = 'Dez';
+
+// Other terms used
+$lang['today'] = 'Hoje';
+$lang['tomorrow'] = 'Amanhã';
+$lang['all_day'] = 'O dia todo';
+$lang['calendar'] = 'Calendário';
+$lang['weather'] = 'Tempo';
+$lang['todo'] = 'Agenda';
+$lang['loading'] = 'Carregando';
+
+?>
