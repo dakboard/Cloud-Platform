cde978e6784f9b5e857fdca84318fd11a2f8545e
swiezaks <31879697+swiezaks@users.noreply.github.com>
2017-09-13 15:14:26 +0200

    Polish translation
    Polish translation for dakboard

---
+++ languages/lang.pl.php
@@ -0,0 +1,61 @@
++<?php
++/* 
++------------------
++Language: Polish
++------------------
++*/
++
++$lang = array();
++
++// Days of the week
++$lang['monday'] = 'Poniedziałek';
++$lang['monday_abbr'] = 'pon';
++$lang['tuesday'] = 'Wtorek';
++$lang['tuesday_abbr'] = 'wt';
++$lang['wednesday'] = 'Środa';
++$lang['wednesday_abbr'] = 'śr';
++$lang['thursday'] = 'Czwartek';
++$lang['thursday_abbr'] = 'czw';
++$lang['friday'] = 'Piątek';
++$lang['friday_abbr'] = 'pt';
++$lang['saturday'] = 'Sobota';
++$lang['saturday_abbr'] = 'sob';
++$lang['sunday'] = 'Niedziela';
++$lang['sunday_abbr'] = 'niedz';
++
++// Months of the year
++$lang['january'] = 'Styczeń';
++$lang['january_abbr'] = 'sty';
++$lang['february'] = 'Luty';
++$lang['february_abbr'] = 'lut';
++$lang['march'] = 'Marzec';
++$lang['march_abbr'] = 'Mar';
++$lang['april'] = 'Kwiecień';
++$lang['april_abbr'] = 'kwi';
++$lang['may'] = 'Maj';
++$lang['may_abbr'] = 'Maj';
++$lang['june'] = 'Czerwiec';
++$lang['june_abbr'] = 'Cze';
++$lang['july'] = 'Lipiec';
++$lang['july_abbr'] = 'Lip';
++$lang['august'] = 'Sierpień';
++$lang['august_abbr'] = 'Sie';
++$lang['september'] = 'Wrzesień';
++$lang['september_abbr'] = 'Wrz';
++$lang['october'] = 'Październik';
++$lang['october_abbr'] = 'Paź';
++$lang['november'] = 'Listopad';
++$lang['november_abbr'] = 'Lis';
++$lang['december'] = 'Grudzień';
++$lang['december_abbr'] = 'Gru';
++
++// Other terms used
++$lang['today'] = 'Dzisiaj';
++$lang['tomorrow'] = 'Jutro';
++$lang['all_day'] = 'Wszystkie dni';
++$lang['calendar'] = 'Kalendarz';
++$lang['weather'] = 'Pogoda';
++$lang['todo'] = 'Do zrobienia';
++$lang['loading'] = 'Wczytuję';
++
++?>
