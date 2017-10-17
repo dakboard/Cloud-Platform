57c2031d2998568e86bae13920bae86eeabf1ee6
kazukata <32877481+kazukata@users.noreply.github.com>
2017-10-18 01:39:52 +0900

    Japanese translation
     Japanese translation for dakboard.

---
+++ languages/lang.ja.php
@@ -0,0 +1,61 @@
+<?php
+/* 
+------------------
+Language: Japanse
+------------------
+*/
+
+$lang = array();
+
+// Days of the week
+$lang['monday'] = '月曜日';
+$lang['monday_abbr'] = '月';
+$lang['tuesday'] = '火曜日';
+$lang['tuesday_abbr'] = '火';
+$lang['wednesday'] = '水曜日';
+$lang['wednesday_abbr'] = '水';
+$lang['thursday'] = '木曜日';
+$lang['thursday_abbr'] = '木';
+$lang['friday'] = '金曜日';
+$lang['friday_abbr'] = '金';
+$lang['saturday'] = '土曜日';
+$lang['saturday_abbr'] = '土';
+$lang['sunday'] = '日曜日';
+$lang['sunday_abbr'] = '日';
+
+// Months of the year
+$lang['january'] = '１月';
+$lang['january_abbr'] = '1月';
+$lang['february'] = '２月';
+$lang['february_abbr'] = '2月';
+$lang['march'] = '３月';
+$lang['march_abbr'] = '3月';
+$lang['april'] = '４月';
+$lang['april_abbr'] = '4月';
+$lang['may'] = '５月';
+$lang['may_abbr'] = '5月';
+$lang['june'] = '６月';
+$lang['june_abbr'] = '6月';
+$lang['july'] = '７月';
+$lang['july_abbr'] = '7月';
+$lang['august'] = '８月';
+$lang['august_abbr'] = '8月';
+$lang['september'] = '９月';
+$lang['september_abbr'] = '9月';
+$lang['october'] = '１０月';
+$lang['october_abbr'] = '10月';
+$lang['november'] = '１１月';
+$lang['november_abbr'] = '11月';
+$lang['december'] = '１２月';
+$lang['december_abbr'] = '12月';
+
+// Other terms used
+$lang['today'] = '今日';
+$lang['tomorrow'] = '明日';
+$lang['all_day'] = '終日';
+$lang['calendar'] = 'カレンダー';
+$lang['weather'] = '天気';
+$lang['todo'] = 'To-do';　//In Japanese, this word is usually written in English
+$lang['loading'] = 'Loading'; //In Japanese, this word is usually written in English
+
+?>
