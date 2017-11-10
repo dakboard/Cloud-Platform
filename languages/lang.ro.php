ad10742b27196c2a66cbedb22d48e16a29eec7be
Victor Ursu <victor.ursu@blinkreaction.com>
2017-11-09 21:14:07 -0500

    Added Romanian language support

---
+++ languages/lang.ro.php
@@ -0,0 +1,60 @@
+<?php
+/* 
+------------------
+Language: Romanian
+------------------
+*/
+
+$lang = array();
+
+// Days of the week
+$lang['monday'] = 'Luni';
+$lang['monday_abbr'] = 'L';
+$lang['tuesday'] = 'Marţi';
+$lang['tuesday_abbr'] = 'Mţ';
+$lang['wednesday'] = 'Miercuri';
+$lang['wednesday_abbr'] = 'Mc';
+$lang['thursday'] = 'Joi';
+$lang['thursday_abbr'] = 'J';
+$lang['friday'] = 'Vineri';
+$lang['friday_abbr'] = 'V';
+$lang['saturday'] = 'Sâmbătă';
+$lang['saturday_abbr'] = 'S';
+$lang['sunday'] = 'Duminică';
+$lang['sunday_abbr'] = 'D';
+
+// Months of the year
+$lang['january'] = 'Ianuarie';
+$lang['january_abbr'] = 'Ian';
+$lang['february'] = 'Februarie';
+$lang['february_abbr'] = 'Feb';
+$lang['march'] = 'Martie';
+$lang['march_abbr'] = 'Mart';
+$lang['april'] = 'Aprilie';
+$lang['april_abbr'] = 'Apr';
+$lang['may'] = 'Mai';
+$lang['may_abbr'] = 'Mai';
+$lang['june'] = 'Iunie';
+$lang['june_abbr'] = 'Iun';
+$lang['july'] = 'Iulie';
+$lang['july_abbr'] = 'Iul';
+$lang['august'] = 'August';
+$lang['august_abbr'] = 'Aug';
+$lang['september'] = 'Septembrie';
+$lang['september_abbr'] = 'Sept';
+$lang['october'] = 'Octombrie';
+$lang['october_abbr'] = 'Oct';
+$lang['november'] = 'Noiembrie';
+$lang['november_abbr'] = 'Nov';
+$lang['december'] = 'Decembrie';
+$lang['december_abbr'] = 'Dec';
+
+// Other terms used
+$lang['today'] = 'Azi';
+$lang['tomorrow'] = 'Mâine';
+$lang['all_day'] = 'Toată ziua';
+$lang['calendar'] = 'Calendar';
+$lang['weather'] = 'Vremea';
+$lang['todo'] = 'Sarcini';
+$lang['loading'] = 'Încărcare';
+?>
