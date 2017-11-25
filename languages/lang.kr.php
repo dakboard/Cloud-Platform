ce87213736fc035f66ed55b2205ceea0a8cfba16
Junhwan Park <semoking@naver.com>
2017-11-26 03:43:58 +0900

    languages: Korean translation
    Signed-off-by: Junhwan Park <semoking@naver.com>

---
+++ languages/lang.kr.php
@@ -0,0 +1,61 @@
+<?php
+/* 
+------------------
+Language: Korean
+------------------
+*/
+
+$lang = array();
+
+// Days of the week
+$lang['monday'] = '월요일';
+$lang['monday_abbr'] = '월';
+$lang['tuesday'] = '화요일';
+$lang['tuesday_abbr'] = '화';
+$lang['wednesday'] = '수요일';
+$lang['wednesday_abbr'] = '수';
+$lang['thursday'] = '목요일';
+$lang['thursday_abbr'] = '목';
+$lang['friday'] = '금요일';
+$lang['friday_abbr'] = '금';
+$lang['saturday'] = '토요일';
+$lang['saturday_abbr'] = '토';
+$lang['sunday'] = '일요일';
+$lang['sunday_abbr'] = '일';
+
+// Months of the year
+$lang['january'] = '1월';
+$lang['january_abbr'] = '1월';
+$lang['february'] = '2월';
+$lang['february_abbr'] = '2월';
+$lang['march'] = '3월';
+$lang['march_abbr'] = '3월';
+$lang['april'] = '4월';
+$lang['april_abbr'] = '4월';
+$lang['may'] = '5월';
+$lang['may_abbr'] = '5월';
+$lang['june'] = '6월';
+$lang['june_abbr'] = '6월';
+$lang['july'] = '7월';
+$lang['july_abbr'] = '7월';
+$lang['august'] = '8월';
+$lang['august_abbr'] = '8월';
+$lang['september'] = '9월';
+$lang['september_abbr'] = '9월';
+$lang['october'] = '10월';
+$lang['october_abbr'] = '10월';
+$lang['november'] = '11월';
+$lang['november_abbr'] = '11월';
+$lang['december'] = '12월';
+$lang['december_abbr'] = '12월';
+
+// Other terms used
+$lang['today'] = '오늘';
+$lang['tomorrow'] = '내일';
+$lang['all_day'] = '하루종일';
+$lang['calendar'] = '캘린더';
+$lang['weather'] = '날씨';
+$lang['todo'] = '할일';
+$lang['loading'] = '준비중';
+
+?>
